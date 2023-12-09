405254adecb06e531fdf20496d391b93c067b69e
Aryeh Hillman <bauernbrot@gmail.com>
2023-12-08 23:14:53 -0800

    Run Linter

--- src/leiningen/add_dep.clj
+++ src/leiningen/add_dep.clj
@@ -1,12 +1,9 @@
 (ns leiningen.add-dep
-  (:require [clojure.pprint :as pprint]
-            [clojure.java.io :as io]
+  (:require [clojure.java.io :as io]
             [clojure.string :as str]
             [zprint.core :as zp]))
 
-(defn print-err [err-msg]
-  (binding [*out* *err*]
-    (println err-msg)))
+(defn print-err [err-msg] (binding [*out* *err*] (println err-msg)))
 
 (defn err!
   "Exits with a non-zero status code and prints the given message to stderr"
@@ -14,16 +11,17 @@
   (print-err err-msg)
   (System/exit 1))
 
-(defn to-leiningen-dep [-maven-coordinate]
-  ;; [group-id/name version] or ["group-id/name" version] 
+(defn to-leiningen-dep
+  [-maven-coordinate]
+  ;; [group-id/name version] or ["group-id/name" version]
   ;; https://github.com/technomancy/leiningen/blob/github/sample.project.clj#L44)
   (let [[group artifact-id version] (str/split -maven-coordinate #":")]
     (vec [(symbol (format "%s/%s" group artifact-id)) version])))
 
-(defn read-project []
-  (vec (read-string (slurp (io/file "project.clj")))))
+(defn read-project [] (vec (read-string (slurp (io/file "project.clj")))))
 
-(defn get-deps-section-idx [project-as-vec]
+(defn get-deps-section-idx
+  [project-as-vec]
   (.indexOf project-as-vec ':dependencies))
 
 (defn get-deps-section
@@ -32,24 +30,31 @@
   (let [deps-idx (get-deps-section-idx project-as-vec)]
     (if (= deps-idx -1) nil (nth project-as-vec (inc deps-idx)))))
 
-(defn set-deps-section [deps-section project-as-vec]
+(defn set-deps-section
+  [deps-section project-as-vec]
   (let [deps-idx (get-deps-section-idx project-as-vec)]
     (if (= deps-idx -1)
       (concat project-as-vec (cons :dependencies (cons deps-section '())))
       (assoc project-as-vec (inc deps-idx) deps-section))))
 
-(defn add-dep [-maven-coordinate project-as-vec]
- ;; (vec nil) := (vec '())
- ;; https://clojuredocs.org/clojure.core/vec
- (set-deps-section
-   (-> (vec (get-deps-section project-as-vec))
-        (merge (to-leiningen-dep -maven-coordinate))) project-as-vec))
-
+(defn add-the-dep
+  [-maven-coordinate project-as-vec]
+  ;; (vec nil) := (vec '())
+  ;; https://clojuredocs.org/clojure.core/vec
+  (->> (set-deps-section (-> (vec (get-deps-section project-as-vec))
+                             (merge (to-leiningen-dep -maven-coordinate)))
+                         project-as-vec)
+       (apply list)))
 
 (def usage "Usage: `lein add-dep com.google.guava:guava:32.1.3-jre`")
 
-;; (defn add-dep
-;;   "I don't do a lot."
-;;   [project & args]
-;;   (if (not (= (count args) 1)) (err! usage))
-;;   (if (not (.exists (io/file "project.clj"))) (err! "project.clj does not exist; exiting.")))
+(defn add-dep
+  "Adds a dependency to `project.clj`"
+  [_project & args]
+  (if (not (= (count args) 1)) (err! usage) nil)
+  (if (not (.exists (io/file "project.clj")))
+    (err! "project.clj does not exist; exiting.")
+    nil)
+  (let [project-as-vec (add-the-dep (nth args 0) (read-project))]
+    (zp/zprint project-as-vec)))
+
--- test/leiningen/add_dep_test.clj
+++ test/leiningen/add_dep_test.clj
@@ -3,12 +3,12 @@
             [clojure.test :refer [deftest is]]))
 
 (deftest empty-no-deps
-  (is 
-    (= (add-dep/add-dep "fun.aryeh:cool:1.2.3" [])
-       [:dependencies [['fun.aryeh/cool "1.2.3"]]])))
+  (is (= (add-dep/add-dep "fun.aryeh:cool:1.2.3" [])
+         [:dependencies [['fun.aryeh/cool "1.2.3"]]])))
 
 (deftest empty-a-dep
-  (is 
-    (= (add-dep/add-dep "fun.aryeh:cool:1.2.3" [:dependencies [['com.example/some-dep "7.8.9"]]])
-       [:dependencies [['com.example/some-dep "7.8.9"] ['fun.aryeh/cool "1.2.3"]]])))
+  (is (= (add-dep/add-dep "fun.aryeh:cool:1.2.3"
+                          [:dependencies [['com.example/some-dep "7.8.9"]]])
+         [:dependencies
+          [['com.example/some-dep "7.8.9"] ['fun.aryeh/cool "1.2.3"]]])))
 
